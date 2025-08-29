c3cb39d4d0f753c5f8f080469072dda2d3a8a3a4
aliaska-varieva <aliaska@flamingo.cx>
2025-08-29 17:44:05 +0200

    Update openframe-data-mongo

---
+++ openframe-data-mongo/src/main/java/com/openframe/сonfig/MongoConfig.java
@@ -0,0 +1,41 @@
+package com.openframe.сonfig;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+public class MongoConfig {
+
+    @Configuration
+    @ConditionalOnProperty(name = "spring.data.mongodb.enabled", havingValue = "true", matchIfMissing = false)
+    @EnableMongoRepositories(basePackages = "com.openframe.data.repository.mongo")
+    public static class MongoConfiguration {
+
+        @Bean
+        public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory factory,
+                                                           MongoMappingContext context,
+                                                           MongoCustomConversions conversions) {
+            DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+            MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, context);
+            converter.setCustomConversions(conversions);
+            converter.setMapKeyDotReplacement("__dot__");
+            return converter;
+        }
+    }
+
+    @Configuration
+    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+    @EnableReactiveMongoRepositories(basePackages = "com.openframe.data.repository.mongo")
+    public static class ReactiveMongoConfiguration {
+    }
+}
