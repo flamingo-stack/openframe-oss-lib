f74f0cbf04d1a7de3d077b23e93a04334148d9f8
aliaska-varieva <aliaska@flamingo.cx>
2025-08-29 14:20:06 +0200

    Update openframe-data-mongo

--- openframe-data-mongo/src/main/java/com/openframe/repository/user/BaseUserRepository.java
+++ openframe-data-mongo/src/main/java/com/openframe/repository/user/BaseUserRepository.java
@@ -4,8 +4,8 @@ package com.openframe.repository.user;
  * Base interface defining common user repository operations.
  * This interface is technology-agnostic and can be implemented by both reactive and non-reactive repositories.
  *
- * @param <T>  The return type wrapper (Optional for blocking, Mono for reactive)
- * @param <B>  The boolean return type (boolean for blocking, Mono<Boolean> for reactive)
+ * @param <T>  The return type wrapper ({@code Optional} for blocking, {@code Mono} for reactive)
+ * @param <B>  The boolean return type ({@code boolean} for blocking, {@code Mono<Boolean>} for reactive)
  * @param <ID> The ID type (String in our case)
  */
 public interface BaseUserRepository<T, B, ID> {
@@ -13,7 +13,7 @@ public interface BaseUserRepository<T, B, ID> {
      * Find a user by their email address.
      *
      * @param email The email address to search for
-     * @return The user wrapped in T (Optional<User> for blocking, Mono<User> for reactive)
+     * @return The user wrapped in T ({@code Optional<User>} for blocking, {@code Mono<User>} for reactive)
      */
     T findByEmail(String email);
 
@@ -23,7 +23,7 @@ public interface BaseUserRepository<T, B, ID> {
      * Blocking repositories should override this.
      *
      * @param email The email to check
-     * @return Boolean (boolean for blocking, Mono<Boolean> for reactive)
+     * @return {@code Boolean} ({@code boolean} for blocking, {@code Mono<Boolean>} for reactive)
      */
     B existsByEmail(String email);
 
@@ -33,7 +33,7 @@ public interface BaseUserRepository<T, B, ID> {
      * Blocking repositories should override this.
      *
      * @param resetToken The reset token to search for
-     * @return The user wrapped in T (Optional<User> for blocking, Mono<User> for reactive)
+     * @return The user wrapped in T ({@code Optional<User>} for blocking, {@code Mono<User>} for reactive)
      */
     T findByResetToken(String resetToken);
 } 
