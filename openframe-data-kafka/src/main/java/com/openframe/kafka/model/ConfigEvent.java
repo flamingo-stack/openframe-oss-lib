f01863e8f618e6839cd5df6abeb827a359fef5e9
Arsenij Malov <arsenij@flamingo.cx>
2025-09-04 22:03:22 +0300

    Add shared kafka producers (for oss and saas)

---
+++ openframe-data-kafka/src/main/java/com/openframe/kafka/model/ConfigEvent.java
@@ -0,0 +1,4 @@
+package com.openframe.kafka.model;
+
+public class ConfigEvent {
+}
---
+++ openframe-data-kafka/src/main/java/com/openframe/kafka/producer/KafkaProducer.java
@@ -0,0 +1,11 @@
+package com.openframe.kafka.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class KafkaProducer extends GenericKafkaProducer {
+
+    public KafkaProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        super(kafkaTemplate);
+    }
+
+}
