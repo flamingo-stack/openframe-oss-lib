1abaffd74130f00389d44cdaa2658c9829772659
aliaska-varieva <aliaska@flamingo.cx>
2025-09-04 14:21:36 +0200

    Fix jackson deserialization issue

--- openframe-data-mongo/src/main/java/com/openframe/data/document/tool/ToolUrl.java
+++ openframe-data-mongo/src/main/java/com/openframe/data/document/tool/ToolUrl.java
@@ -1,10 +1,14 @@
 package com.openframe.data.document.tool;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ToolUrl {
     private String url;
     private String port;
