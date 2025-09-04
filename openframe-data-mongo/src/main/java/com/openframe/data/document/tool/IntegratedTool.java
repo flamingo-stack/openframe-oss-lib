12572cd1ff267eccfd6aa31d9e9bb657240b8568
aliaska-varieva <aliaska@flamingo.cx>
2025-09-04 14:24:13 +0200

    Fix jackson deserialization issue (#13)

--- openframe-data-mongo/src/main/java/com/openframe/data/document/tool/IntegratedTool.java
+++ openframe-data-mongo/src/main/java/com/openframe/data/document/tool/IntegratedTool.java
@@ -1,7 +1,9 @@
 package com.openframe.data.document.tool;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,6 +11,8 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "integrated_tools")
 public class IntegratedTool {
     @Id
