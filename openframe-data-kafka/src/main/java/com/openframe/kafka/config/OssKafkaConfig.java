28df88bd8fad47d4b56a4fb05d1fbb94037ca882
Arsenij Malov <arsenij@flamingo.cx>
2025-09-15 19:04:12 +0300

    clean

--- openframe-data-kafka/src/main/java/com/openframe/kafka/config/OssKafkaConfig.java
+++ openframe-data-kafka/src/main/java/com/openframe/kafka/config/OssKafkaConfig.java
@@ -1,6 +1,5 @@
 package com.openframe.kafka.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.kafka.annotation.EnableKafka;
