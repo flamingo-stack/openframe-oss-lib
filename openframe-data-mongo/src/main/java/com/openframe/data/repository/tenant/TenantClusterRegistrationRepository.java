2d2b52867e0949f287a6c1ff1f250a3a9c109431
aliaska-varieva <aliaska@flamingo.cx>
2025-09-20 21:33:13 +0200

    Add existsByTenantIdIsNull, findFirstByTenantIdIsNull

--- openframe-data-mongo/src/main/java/com/openframe/data/repository/tenant/TenantClusterRegistrationRepository.java
+++ openframe-data-mongo/src/main/java/com/openframe/data/repository/tenant/TenantClusterRegistrationRepository.java
@@ -3,7 +3,10 @@ package com.openframe.data.repository.tenant;
 import com.openframe.data.document.tenant.TenantClusterRegistration;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface TenantClusterRegistrationRepository extends MongoRepository<TenantClusterRegistration, String> {
+    boolean existsByTenantIdIsNull();
+    Optional<TenantClusterRegistration> findFirstByTenantIdIsNull();
 }
