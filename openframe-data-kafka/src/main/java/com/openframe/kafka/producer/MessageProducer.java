d8ca0ef395318694ffe66310ed610c356466a9c7
Arsenij Malov <arsenij@flamingo.cx>
2025-09-24 22:47:58 +0300

    Delete saas kafka

--- openframe-data-kafka/src/main/java/com/openframe/kafka/producer/MessageProducer.java
+++ openframe-data-kafka/src/main/java/com/openframe/kafka/producer/MessageProducer.java
@@ -2,7 +2,7 @@ package com.openframe.kafka.producer;
 
 import com.openframe.kafka.model.KafkaMessage;
 
-public interface OssTenantMessageProducer {
+public interface MessageProducer {
 
     void sendMessage(String messageDestinationName, KafkaMessage message, String specificKey);
 
