69c32522ff11a7c6a5624be5f75be01cd8bf178f
Kirill Gontar <Kirill Gontar>
2025-11-07 15:38:38 +0100

    Link config

---
+++ openframe-data-mongo/src/main/java/com/openframe/data/document/clientconfiguration/DownloadLinkData.java
@@ -0,0 +1,11 @@
+package com.openframe.data.document.clientconfiguration;
+
+import lombok.Data;
+
+@Data
+public class DownloadLinkData {
+
+    private String os;
+    private String linkTemplate;
+
+}
