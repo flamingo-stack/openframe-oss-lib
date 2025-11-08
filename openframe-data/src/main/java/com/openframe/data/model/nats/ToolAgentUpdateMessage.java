d8eff220618346d0b247f27cfd7ed5aa443398b5
Kirill Gontar <Kirill Gontar>
2025-11-08 18:19:05 +0100

    Publish download configuration for tools and client update

--- openframe-data/src/main/java/com/openframe/data/model/nats/ToolAgentUpdateMessage.java
+++ openframe-data/src/main/java/com/openframe/data/model/nats/ToolAgentUpdateMessage.java
@@ -3,11 +3,14 @@ package com.openframe.data.model.nats;
 import com.openframe.data.document.toolagent.SessionType;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ToolAgentUpdateMessage {
   
     private String toolAgentId;
     private String version;
     private SessionType sessionType;
+    private List<DownloadConfiguration> downloadConfigurations;
     
 }
