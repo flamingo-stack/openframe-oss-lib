d87738ea340dde8a26dd6816a5d292e4764c6c09
Kirill Gontar <Kirill Gontar>
2025-11-08 17:24:18 +0100

    Repository

---
+++ openframe-data-mongo/src/main/java/com/openframe/data/repository/installedagents/InstalledAgentRepository.java
@@ -0,0 +1,14 @@
+package com.openframe.data.repository.installedagents;
+
+import com.openframe.data.document.installedagents.InstalledAgent;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface InstalledAgentRepository extends MongoRepository<InstalledAgent, String> {
+
+    Optional<InstalledAgent> findByMachineId(String machineId);
+
+}
