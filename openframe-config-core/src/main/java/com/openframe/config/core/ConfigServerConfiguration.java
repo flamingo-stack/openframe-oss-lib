2e2b93d63e0b72e123f0aa03c964f69b7bd5a445
aliaska-varieva <aliaska@flamingo.cx>
2025-11-12 03:19:26 +0100

    Move config server core functionality to libs

---
+++ openframe-config-core/src/main/java/com/openframe/config/core/ConfigServerConfiguration.java
@@ -0,0 +1,9 @@
+package com.openframe.config.core;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfiguration {
+}
