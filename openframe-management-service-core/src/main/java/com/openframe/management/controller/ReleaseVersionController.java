94673285d62e4595c178bcc1202354bb9ba52cac
kirill-567 <kirill@flamingo.cx>
2025-11-27 16:49:54 +0100

    Store release version at the tenant

---
+++ openframe-management-service-core/src/main/java/com/openframe/management/controller/ReleaseVersionController.java
@@ -0,0 +1,22 @@
+package com.openframe.management.controller;
+
+import com.openframe.management.dto.ReleaseVersionRequest;
+import com.openframe.management.service.ReleaseVersionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/cluster-registrations")
+@RequiredArgsConstructor
+public class ReleaseVersionController {
+
+    private final ReleaseVersionService releaseVersionService;
+
+    @PostMapping
+    public void updateReleaseVersion(@RequestBody ReleaseVersionRequest request) {
+        releaseVersionService.process(request.getImageTagVersion());
+    }
+}
+
