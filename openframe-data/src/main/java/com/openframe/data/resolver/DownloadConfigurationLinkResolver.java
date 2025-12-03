af8a72ad1391348eecfb6982d1d34be4c0b8e153
kirill-567 <kirill@flamingo.cx>
2025-12-03 14:54:53 +0100

    Process latest version release for clients

--- openframe-data/src/main/java/com/openframe/data/resolver/DownloadConfigurationLinkResolver.java
+++ openframe-data/src/main/java/com/openframe/data/resolver/DownloadConfigurationLinkResolver.java
@@ -11,13 +11,28 @@ import java.util.Map;
 @Slf4j
 public class DownloadConfigurationLinkResolver {
 
+    private static final String LATEST_VERSION_TAG = "latest";
+    private static final String VERSION_TAG_PATH = "download/{version}";
+    private static final String LATEST_VERSION_TAG_PATH = "latest/download";
     private static final String VERSION_PLACEHOLDER = "{version}";
+
 
     public String resolve(DownloadConfiguration config, String version) {
         String linkTemplate = config.getLinkTemplate();
-        String resolvedLink = linkTemplate.replace(VERSION_PLACEHOLDER, version);
+        String resolvedLink = resolveLink(linkTemplate, version);
         log.debug("Resolved link template '{}' to '{}'", linkTemplate, resolvedLink);
         return resolvedLink;
+    }
+
+    private String resolveLink(String linkTemplate, String version) {
+        if (isLatestVersion(version)) {
+            return linkTemplate.replace(VERSION_TAG_PATH, LATEST_VERSION_TAG_PATH);
+        }
+        return linkTemplate.replace(VERSION_PLACEHOLDER, version);
+    }
+
+    private boolean isLatestVersion(String version) {
+        return version.equals(LATEST_VERSION_TAG);
     }
     
 }
