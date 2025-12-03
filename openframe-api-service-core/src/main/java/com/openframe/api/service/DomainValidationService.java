7d06ca9097abdd6d5dac15c914fb659a321c3d4f
aliaska-varieva <aliaska@flamingo.cx>
2025-12-03 14:12:59 +0100

    Modify GENERIC_PUBLIC_DOMAINS list (#194)

--- openframe-api-service-core/src/main/java/com/openframe/api/service/DomainValidationService.java
+++ openframe-api-service-core/src/main/java/com/openframe/api/service/DomainValidationService.java
@@ -13,21 +13,81 @@ public class DomainValidationService {
     private final DomainExistenceValidator domainExistenceValidator;
 
     private static final Set<String> GENERIC_PUBLIC_DOMAINS = Set.of(
+            // Google
             "gmail.com",
             "googlemail.com",
+            // Yahoo
             "yahoo.com",
+            // Microsoft
             "outlook.com",
             "hotmail.com",
             "live.com",
             "msn.com",
+            // Apple
             "icloud.com",
             "me.com",
             "mac.com",
+            // Other global
             "aol.com",
             "gmx.com",
+            "gmx.net",
             "proton.me",
             "protonmail.com",
-            "fastmail.com"
+            "fastmail.com",
+            "mail.com",
+            "zoho.com",
+            "tutanota.com",
+            "tuta.com",
+            "yandex.com",
+            "mail.ru",
+            "inbox.com",
+            // China
+            "qq.com",
+            "163.com",
+            "126.com",
+            // Korea
+            "naver.com",
+            "daum.net",
+            "hanmail.net",
+            // Germany
+            "web.de",
+            "gmx.de",
+            "t-online.de",
+            // Italy
+            "libero.it",
+            // Poland
+            "wp.pl",
+            "o2.pl",
+            "onet.pl",
+            // Czech
+            "seznam.cz",
+            // Russia
+            "rambler.ru",
+            // Ukraine
+            "ukr.net",
+            "i.ua",
+            "meta.ua",
+            // France
+            "orange.fr",
+            "laposte.net",
+            // UK
+            "btinternet.com",
+            "sky.com",
+            // US ISPs
+            "att.net",
+            "sbcglobal.net",
+            "verizon.net",
+            "comcast.net",
+            "cox.net",
+            "charter.net",
+            "earthlink.net",
+            // Privacy-focused
+            "mailbox.org",
+            "posteo.de",
+            "hushmail.com",
+            "mailfence.com",
+            "startmail.com",
+            "runbox.com"
     );
 
     public void validateExists(List<String> domains) {
