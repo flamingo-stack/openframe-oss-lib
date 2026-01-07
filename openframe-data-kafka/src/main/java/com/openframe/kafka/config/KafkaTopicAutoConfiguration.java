4be08de83047fc8cd5992d5afd658c9fe2ddaf4d
Arsenij Malov <arsenij@flamingo.cx>
2026-01-07 19:37:54 +0200

    kafka topic initializer

--- openframe-data-kafka/src/main/java/com/openframe/kafka/config/KafkaTopicAutoConfiguration.java
+++ openframe-data-kafka/src/main/java/com/openframe/kafka/config/KafkaTopicAutoConfiguration.java
@@ -1,15 +1,13 @@
 package com.openframe.kafka.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration(after = OssTenantKafkaAutoConfiguration.class)
-@EnableConfigurationProperties(KafkaTopicProperties.class)
+@EnableConfigurationProperties({KafkaTopicProperties.class, OssTenantKafkaProperties.class})
 @ConditionalOnProperty(prefix = "spring.oss-tenant.kafka", name = "enabled", havingValue = "true")
-@ConditionalOnBean(OssTenantKafkaProperties.class)
 public class KafkaTopicAutoConfiguration {
 
     @Bean
