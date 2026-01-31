c60536fa597460bb08261f83b162421a7728377c
Kirill Gontar <Kirill Gontar>
2026-01-31 19:11:20 +0100

    Update file name

--- openframe-data-mongo/src/main/java/com/openframe/data/document/toolagent/ToolAgentAsset.java
+++ openframe-data-mongo/src/main/java/com/openframe/data/document/toolagent/ToolAgentAsset.java
@@ -3,12 +3,14 @@ package com.openframe.data.document.toolagent;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class ToolAgentAsset {
     
     private String id;
-    private String localFilename;
+    private List<LocalFilenameConfiguration> localFilenameConfiguration;
     private ToolAgentAssetSource source;
     private String path;
     private boolean executable;
