35e423a21203e48be8779a8c389b4f62a42a94f9
aliaska-varieva <aliaska@flamingo.cx>
2026-03-06 00:14:21 +0100

    Refactor exception lib, exclude security errors (#638)

--- openframe-api-service-core/src/main/java/com/openframe/api/exception/GlobalExceptionHandler.java
+++ openframe-api-service-core/src/main/java/com/openframe/api/exception/GlobalExceptionHandler.java
@@ -4,18 +4,17 @@ import com.openframe.core.dto.ErrorResponse;
 import com.openframe.core.exception.BaseGlobalExceptionHandler;
 import com.openframe.core.exception.EncryptionException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends BaseGlobalExceptionHandler {
 
     @ExceptionHandler(EncryptionException.class)
-    public ResponseEntity<ErrorResponse> handleEncryptionException(EncryptionException ex, WebRequest request) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleEncryptionException(EncryptionException ex) {
         log.error("Encryption/decryption error: ", ex);
-        return buildResponse(ex.getErrorCode(), "Configuration security error",
-                HttpStatus.INTERNAL_SERVER_ERROR, request);
+        return buildResponse(ex.getErrorCode(), "Configuration security error");
     }
 }
--- openframe-client-core/src/main/java/com/openframe/client/exception/GlobalExceptionHandler.java
+++ openframe-client-core/src/main/java/com/openframe/client/exception/GlobalExceptionHandler.java
@@ -4,19 +4,18 @@ import com.openframe.core.dto.ErrorResponse;
 import com.openframe.core.exception.BaseGlobalExceptionHandler;
 import com.openframe.core.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends BaseGlobalExceptionHandler {
 
     @ExceptionHandler(AgentRegistrationSecretValidationErrorException.class)
-    public ResponseEntity<ErrorResponse> handleAgentRegistrationSecretValidationErrorException(
-            AgentRegistrationSecretValidationErrorException ex, WebRequest request) {
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleAgentRegistrationSecretValidationErrorException(
+            AgentRegistrationSecretValidationErrorException ex) {
         log.error("Invalid agent initial key: ", ex);
-        return buildResponse(ErrorCode.UNAUTHORIZED, "Internal server error",
-                HttpStatus.UNAUTHORIZED, request);
+        return buildResponse(ErrorCode.UNAUTHORIZED, "Internal server error");
     }
 }
