4169f03ca1913afc9a2686296dff9e3d88351fc8
Kirill Gontar <Kirill Gontar>
2026-03-06 15:02:54 +0100

    done

--- sdk/fleetmdm/src/main/java/com/openframe/sdk/fleetmdm/model/CreateUserRequest.java
+++ sdk/fleetmdm/src/main/java/com/openframe/sdk/fleetmdm/model/CreateUserRequest.java
@@ -1,7 +1,11 @@
 package com.openframe.sdk.fleetmdm.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class CreateUserRequest {
 
     private String name;
@@ -14,18 +18,4 @@ public class CreateUserRequest {
     @JsonProperty("api_only")
     private boolean apiOnly;
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getGlobalRole() { return globalRole; }
-    public void setGlobalRole(String globalRole) { this.globalRole = globalRole; }
-
-    public boolean isApiOnly() { return apiOnly; }
-    public void setApiOnly(boolean apiOnly) { this.apiOnly = apiOnly; }
 }
