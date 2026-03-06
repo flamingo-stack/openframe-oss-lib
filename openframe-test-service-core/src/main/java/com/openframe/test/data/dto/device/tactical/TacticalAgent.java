2e922e1037ad87194d94750ae59d72bb0dc7ef07
gio <122273656+giokur@users.noreply.github.com>
2026-03-06 12:39:51 +0100

    Test/update device queries (#647)

---
+++ openframe-test-service-core/src/main/java/com/openframe/test/data/dto/device/tactical/TacticalAgent.java
@@ -0,0 +1,16 @@
+package com.openframe.test.data.dto.device.tactical;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TacticalAgent {
+    private String status;
+}
