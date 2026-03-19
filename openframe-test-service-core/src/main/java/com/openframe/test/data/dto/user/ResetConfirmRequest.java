581daaa0fa37e8ed8c1004dd4a77f65e0829cf9e
gio <122273656+giokur@users.noreply.github.com>
2026-03-19 10:21:50 +0100

    do not serialize nulls (#698)

--- openframe-test-service-core/src/main/java/com/openframe/test/data/dto/user/ResetConfirmRequest.java
+++ openframe-test-service-core/src/main/java/com/openframe/test/data/dto/user/ResetConfirmRequest.java
@@ -1,5 +1,6 @@
 package com.openframe.test.data.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResetConfirmRequest {
     private String token;
     private String newPassword;
