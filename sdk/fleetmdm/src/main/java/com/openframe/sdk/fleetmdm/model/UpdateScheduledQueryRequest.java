cf1354c514ea8eedaa89b503a7c4f597b8e7f412
kirill-567 <kirill@flamingo.cx>
2026-04-18 22:28:00 +0200

    Script/ScheduledScript/Query/Policy api client

---
+++ sdk/fleetmdm/src/main/java/com/openframe/sdk/fleetmdm/model/UpdateScheduledQueryRequest.java
@@ -0,0 +1,18 @@
+package com.openframe.sdk.fleetmdm.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UpdateScheduledQueryRequest {
+    private String name;
+    private String query;
+    private String description;
+    private Integer interval;
+    private String platform;
+}
---
+++ sdk/tacticalrmm/src/main/java/com/openframe/sdk/tacticalrmm/model/UpdateScriptScheduleRequest.java
@@ -0,0 +1,38 @@
+package com.openframe.sdk.tacticalrmm.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UpdateScriptScheduleRequest {
+
+    private String name;
+
+    @JsonProperty("task_type")
+    private String taskType;
+
+    @JsonProperty("run_time_date")
+    private String runTimeDate;
+
+    private Boolean enabled;
+
+    @JsonProperty("run_time_bit_weekdays")
+    private Integer runTimeBitWeekdays;
+
+    @JsonProperty("weekly_interval")
+    private Integer weeklyInterval;
+
+    @JsonProperty("daily_interval")
+    private Integer dailyInterval;
+
+    @JsonProperty("task_supported_platforms")
+    private List<String> taskSupportedPlatforms;
+
+    private List<TaskAction> actions;
+}
