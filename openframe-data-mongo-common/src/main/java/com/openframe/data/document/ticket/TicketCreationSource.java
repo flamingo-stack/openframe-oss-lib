6a4f551cef119b6a501308e2d2c0c13e6037b21e
Arsenij Malov <arsenij@flamingo.cx>
2026-05-12 15:17:52 +0200

    custom statuses

--- openframe-data-mongo-common/src/main/java/com/openframe/data/document/ticket/TicketCreationSource.java
+++ openframe-data-mongo-common/src/main/java/com/openframe/data/document/ticket/TicketCreationSource.java
@@ -1,11 +1,8 @@
 package com.openframe.data.document.ticket;
 
-/**
- * How the ticket was created.
- * Auto-populated by system based on creation flow.
- */
 public enum TicketCreationSource {
     FAE_FORM,
     FAE_DIALOG,
-    ADMIN_DASHBOARD
+    ADMIN_DASHBOARD,
+    INBOUND_EMAIL
 }
