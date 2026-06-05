19d971b3bd4bf4689403792a4b35e58381974529
Dmytro Koval <dmytro@flamingo.cx>
2026-06-05 21:55:33 +0200

    Remove NotificationContextSelectiveTypeMapper to fix polymorphic disp… (#1126)

--- openframe-data-mongo-sync/src/main/java/com/openframe/data/config/NotificationContextMongoConfig.java
+++ openframe-data-mongo-sync/src/main/java/com/openframe/data/config/NotificationContextMongoConfig.java
@@ -3,11 +3,9 @@ package com.openframe.data.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openframe.data.repository.notification.NotificationContextReadConverter;
 import com.openframe.data.repository.notification.NotificationContextWriteConverter;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 import java.util.List;
@@ -31,18 +29,5 @@ public class NotificationContextMongoConfig {
             NotificationContextReadConverter readConverter,
             NotificationContextWriteConverter writeConverter) {
         return new MongoCustomConversions(List.of(readConverter, writeConverter));
-    }
-
-    @Bean
-    public static BeanPostProcessor notificationContextTypeMapperPostProcessor() {
-        return new BeanPostProcessor() {
-            @Override
-            public Object postProcessAfterInitialization(Object bean, String beanName) {
-                if (bean instanceof MappingMongoConverter converter) {
-                    converter.setTypeMapper(new NotificationContextSelectiveTypeMapper());
-                }
-                return bean;
-            }
-        };
     }
 }
