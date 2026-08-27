8bd764f2c84d08b9b8648f21b759aa106faacad8
hryhorii-chuhuievets <hryhorii@flamingo.cx>
2026-08-27 12:39:58 +0300

    Add new NotificationSettingGroup.TICKET_CREATED (#1920)
    https://app.clickup.com/t/9013925967/86ak5mttx

--- openframe-data-mongo-common/src/main/java/com/openframe/data/document/notification/NotificationSettingGroup.java
+++ openframe-data-mongo-common/src/main/java/com/openframe/data/document/notification/NotificationSettingGroup.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum NotificationSettingGroup {
     TICKET_ASSIGNED("Ticket assigned"),
+    TICKET_CREATED("Ticket created"),
     TICKET_STATUS_CHANGED("Ticket status changed"),
     CUSTOMER_REPLIED("Customer replied"),
     ADMIN_REPLIED("Admin replied"),
