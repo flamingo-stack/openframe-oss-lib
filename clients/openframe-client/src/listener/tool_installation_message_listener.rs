d407a92e8c3b5f5d57dac3e1cb2743b7745208b8
denys-gif <denys@flamingo.cx>
2026-08-31 18:23:26 +0100

    fix: nats connection watchdog

--- clients/openframe-client/src/listener/tool_installation_message_listener.rs
+++ clients/openframe-client/src/listener/tool_installation_message_listener.rs
@@ -73,14 +73,20 @@ impl ToolInstallationMessageListener {
     async fn listen(&self) -> Result<()> {
         info!("Run tool installation message listener");
         let machine_id = self.config_service.get_machine_id()?;
-        let reprovisioning = Arc::new(AtomicBool::new(false));
-
         loop {
+            let reprovisioning = Arc::new(AtomicBool::new(false));
+            let mut client_rx = self.nats_connection_manager.on_client_replaced();
             let client = self.nats_connection_manager.get_client().await?;
             let mut reconnect_rx = self.nats_connection_manager.subscribe_reconnect();
             let js = jetstream::new((*client).clone());
 
-            let consumer = self.create_consumer(&js, &machine_id).await;
+            let consumer = tokio::select! {
+                consumer = self.create_consumer(&js, &machine_id) => consumer,
+                _ = client_rx.changed() => {
+                    info!("NATS client replaced, rebinding tool installation consumer");
+                    continue;
+                }
+            };
 
             info!("Start listening for tool installation messages");
             let mut messages = consumer.messages().await?;
@@ -104,6 +110,10 @@ impl ToolInstallationMessageListener {
                             }
                         }
                     }
+                    _ = client_rx.changed() => {
+                        info!("NATS client replaced, rebinding tool installation consumer");
+                        break;
+                    }
                     _ = reconnect_rx.recv() => {
                         info!("NATS reconnected, re-provisioning tool installation consumer");
                         if !reprovisioning.swap(true, Ordering::SeqCst) {
@@ -111,8 +121,12 @@ impl ToolInstallationMessageListener {
                             let js = js.clone();
                             let machine_id = machine_id.clone();
                             let reprovisioning = reprovisioning.clone();
+                            let mut client_rx = client_rx.clone();
                             tokio::spawn(async move {
-                                listener.create_consumer(&js, &machine_id).await;
+                                tokio::select! {
+                                    _ = listener.create_consumer(&js, &machine_id) => {}
+                                    _ = client_rx.changed() => {}
+                                }
                                 reprovisioning.store(false, Ordering::SeqCst);
                             });
                         }
