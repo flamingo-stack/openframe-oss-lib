111c0a9fab50fd8e23a5c47b255836e676a71ac7
Kirill Gontar <Kirill Gontar>
2026-09-20 15:04:31 +0200

    todo

--- openframe-data-mongo-common/src/main/java/com/openframe/data/document/assignment/AssignmentItemType.java
+++ openframe-data-mongo-common/src/main/java/com/openframe/data/document/assignment/AssignmentItemType.java
@@ -3,5 +3,6 @@ package com.openframe.data.document.assignment;
 public enum AssignmentItemType {
     TICKET,
     KNOWLEDGE_ARTICLE,
+    // TODO: insight is a SaaS concept and should not be named here.
     INSIGHT
 }
